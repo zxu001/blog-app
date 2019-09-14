4a32f3b82b05f863a115381694336cb5128d8b0e
John Xu <jxu0099@gmail.com>
2019-09-14 14:36:49 -0400

    forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',  #new
+    'accounts.apps.AccountsConfig',  #new
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL ='home'
+LOGOUT_REDIRECT_URL ='home'  #new
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include #new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),   #new
+    path('accounts/', include('accounts.urls')),  #new
     path('', include('blog.urls')),  #new
 ]
